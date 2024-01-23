97cd541aefcf7404645f9790fc57c000198ecf0d
Francisco Monteiro <fmonteiro@sentilant.com>
2024-01-23 20:36:50 +0000

    Create some packages and rename resources

--- src/main/java/org/fmmonteiro/resource/ShipResource.java
+++ src/main/java/org/fmmonteiro/resource/ShipResource.java
@@ -1,16 +1,16 @@
-package org.fmmonteiro;
+package org.fmmonteiro.resource;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
-public class GreetingResource {
+@Path("/ships")
+public class ShipResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello Kakau!";
+    public String retrieveShips() {
+        return "Retrieving all ships...";
     }
 }
